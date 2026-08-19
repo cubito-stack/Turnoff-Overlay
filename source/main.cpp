7b651ee919643067d54a42eaa537a88a5a1077da
cubito-stack <cubitoxdu@gmail.com>
2026-08-18 23:01:04 -0400

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,92 +1,66 @@
-#define TESLA_INIT_IMPL // If you have more than one file using the tesla header, only define this in the main one
-#include <tesla.hpp>    // The Tesla Header
-
+#define TESLA_INIT_IMPL // Solo definir en el archivo principal
+#include <tesla.hpp>    // Libtesla Header
 
 class GuiTest : public tsl::Gui {
 public:
     GuiTest(u8 arg1, u8 arg2, bool arg3) { }
 
-    // Called when this Gui gets loaded to create the UI
-    // Allocate all elements on the heap. libtesla will make sure to clean them up when not needed anymore
     virtual tsl::elm::Element* createUI() override {
-        // A OverlayFrame is the base element every overlay consists of. This will draw the default Title and Subtitle.
-        // If you need more information in the header or want to change it's look, use a HeaderOverlayFrame.
         auto frame = new tsl::elm::OverlayFrame("Turn Off Overlay", "v0.1");
-
-        // A list that can contain sub elements and handles scrolling
         auto list = new tsl::elm::List();
 
-        // List Items
         list->addItem(new tsl::elm::CategoryHeader("Options"));
 
-        //shutdown
-        auto *clickableListItem = new tsl::elm::ListItem("Turn off", "Save ur data before");
+        // Apagar consola (Shutdown)
+        auto *clickableListItem = new tsl::elm::ListItem("Turn off", "Save your data before");
         clickableListItem->setClickListener([](u64 keys) {
             if (keys & HidNpadButton_A) {
-                splExit();
-                fsdevUnmountAll();
-                spsmShutdown(false); //remember kids false = shutdown true = restart
+                spsmShutdown(false); // false = Shutdown
                 return true;
             }
-
             return false;
         });
         list->addItem(clickableListItem);
 
-        //shutdown
-        auto *clickableListItem2 = new tsl::elm::ListItem("Restart", "save or lose ur vbucks");
+        // Reiniciar consola (Restart)
+        auto *clickableListItem2 = new tsl::elm::ListItem("Restart", "Save your progress");
         clickableListItem2->setClickListener([](u64 keys) {
             if (keys & HidNpadButton_A) {
-                splExit();
-                fsdevUnmountAll();
-                spsmShutdown(true); //remember kids false = shutdown true = restart
-                return true; //i clicked a i swear
+                spsmShutdown(true); // true = Restart
+                return true;
             }
-
             return false;
         });
         list->addItem(clickableListItem2);
 
-
-        // Add the list to the frame for it to be drawn
         frame->setContent(list);
-
-        // Return the frame to have it become the top level element of this Gui
         return frame;
     }
 
-    // Called once every frame to update values
-    virtual void update() override {
+    virtual void update() override {}
 
-    }
-
-    // Called once every frame to handle inputs not handled by other UI elements
     virtual bool handleInput(u64 keysDown, u64 keysHeld, const HidTouchState &touchPos, HidAnalogStickState joyStickPosLeft, HidAnalogStickState joyStickPosRight) override {
-        return false;   // Return true here to signal the inputs have been consumed
+        return false;
     }
 };
 
-/*
-SOMETHING VERY IMPORTANT ABOUT initServices()
-This may seem obvious but i was confused for 2 minutes before i realized this.
-It inits the services when you START atmosphere, not when you enter to the overlay...
-This means you'll need to restart the console for the first use of the services and ALSO when you add new services to the code (obv)-
-This isn't a big deal but in a big overlay developing when u are testing ur code and stuff this will be a little tedious... 😓
-*/
-
 class OverlayTest : public tsl::Overlay {
 public:
-                                             // libtesla already initialized fs, hid, pl, pmdmnt, hid:sys and set:sys
+    // Inicializar servicios necesarios
     virtual void initServices() override {
-        spsmInitialize(); //init spsm for the pure purpose to shutdown the console, bless spsm btw
-    }  // Called at the start to initialize all services necessary for this Overlay
-    virtual void exitServices() override {}  // Called at the end to clean up all services previously initialized
+        spsmInitialize(); 
+    }
 
-    virtual void onShow() override {}    // Called before overlay wants to change from invisible to visible state
-    virtual void onHide() override {}    // Called before overlay wants to change from visible to invisible state
+    // Limpiar servicios al salir
+    virtual void exitServices() override {
+        spsmExit();
+    }
+
+    virtual void onShow() override {}
+    virtual void onHide() override {}
 
     virtual std::unique_ptr<tsl::Gui> loadInitialGui() override {
-        return initially<GuiTest>(1, 2, true);  // Initial Gui to load. It's possible to pass arguments to it's constructor like this
+        return initially<GuiTest>(1, 2, true);
     }
 };
 
